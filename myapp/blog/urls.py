1003965206b6e663865dd49c97f1082ad0f10d29
unknown <niranjan20032021@gmail.com>
2024-10-17 21:09:55 +0530

    basic configuration

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name='blog'
 
 urlpatterns=[
     path('details/<int:id>',views.detail, name='detail'),
     path('index',views.index, name= 'index'),
-    path('new_url',views., name= 'index'),
+    path('new_url',views.new_url_view, name= 'new_page_url'),
+    path('old_url',views.old_url_redirect, name= 'old_url'),
 
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls import reverse
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!, You are in blog's index page")
+    return render(request,'index.html')
 
 def detail(request,id):
     return HttpResponse(f"You are viewing post detail page. And the the ID is {id}")
     
 def old_url_redirect(request):
-    return redirect("new_url")
+    return redirect(reverse('blog:new_page_url'))
 
 def new_url_view(request):
     return HttpResponse("This is the new urls")
