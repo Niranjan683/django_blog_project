f9e038ff613480e100eab2c3739e192aa70adcdb
unknown <niranjan20032021@gmail.com>
2024-10-29 15:54:37 +0530

    for if and basic configs are updated

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,7 +5,15 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    blog_title = "Latest Posts" 
+    posts=[
+        {'id':1,'title':'Post 1', 'Content': " Content of Post 1"},
+        {'id':2,'title':'Post 2', 'Content': " Content of Post 2"},
+        {'id':3,'title':'Post 3', 'Content': " Content of Post 3"},
+        {'id':4,'title':'Post 4', 'Content': " Content of Post 4"},
+
+    ]
+    return render(request,'index.html',{'blog_title': blog_title, 'posts' : posts})
 
 def detail(request,id):
     return render (request,'detail.html')    
